86d5b856f05c8e24560371ce5610a4b8fddab3ed
Madhvendra Singh <130988935+Madjob23@users.noreply.github.com>
2023-10-06 20:39:14 +0530

    delete script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-import { motion, useAnimation } from "framer-motion";
 
-const projectItems = document.querySelectorAll(".project");
-
-projectItems.forEach((item) => {
-    const controls = useAnimation();
-
-    item.addEventListener("mouseenter", () => {
-        controls.start({ scale: 2 });
-    });
-
-    item.addEventListener("mouseleave", () => {
-        controls.start({ scale: 1 });
-    });
-
-    const AnimatedProjectItem = motion(item);
-    AnimatedProjectItem.controls = controls;
-});
